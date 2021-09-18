4c7f7a55301d3f148a918d31dc4a30d4663f56e4
Arjun <arjun2000raj@gmail.com>
2021-09-19 04:06:41 +0530

    css made

--- src/component/IsTriangle.js
+++ src/component/IsTriangle.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import './IsTriangle.css'
 
 const IsTriangle = () => {
     return (
-        <div className="container">
-            is triangle...
+        <div className="container isTriangle">
+            <h1> Angles of triangles</h1>
+
+            <label htmlFor="angle1">Angle1</label>
+            <input type="text" placeholder="Enter value" id="angle1" />
+            <label htmlFor="angle2">Angle2</label>
+            <input type="text" placeholder="Enter value" id="angle2" />
+            <label htmlFor="angle3">Angle3</label>
+            <input type="text" placeholder="Enter value" id="angle3" />
+            <button>isTriangle</button>
         </div>
     )
 }
