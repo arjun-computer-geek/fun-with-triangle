b849a60d9e50733c09c18eb9077ffff7f5b8854b
Arjun <arjun2000raj@gmail.com>
2021-09-19 11:39:27 +0530

    area of triangle finished

--- src/component/AreaOfTriangle.js
+++ src/component/AreaOfTriangle.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './AreaOfTriangle.css'
 
 const AreaOfTriangle = () => {
@@ -6,24 +6,25 @@ const AreaOfTriangle = () => {
     const [base, setBase] = useState('');
     const [height, setHeight] = useState('');
 
-    const calculateArea = () => {
+    useEffect(() => {
         if(base !== '' || base !== ''){
             let b = Number(base)
             let h = Number(height)
-            let area = (b * h);
+            let area = (b * h) /2;
             setArea(area)
         }
         
-    }
+    }, [area,base,height])
+
     return (
         <div className="container area-of-triangle-container">
             <p id="output">A = {area ? area : 'h.b/2'}  </p>
 
             <label htmlFor="base">b <span>Base</span></label>
-            <input type="text" placeholder="Enter value" id="base" onChange={event => {setBase(event.target.value); calculateArea()}} />
+            <input type="text" placeholder="Enter value" id="base" onChange={event => setBase(event.target.value)} />
             <br/>
             <label htmlFor="height">h <span>Height</span></label>
-            <input type="text" placeholder="Enter value" id="height" onChange={event => {setHeight(event.target.value); calculateArea()}} />
+            <input type="text" placeholder="Enter value" id="height" onChange={event => setHeight(event.target.value)} />
 
         </div>
     )
